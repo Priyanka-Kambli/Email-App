37b6c7d581987dc1c935d594f75999f1cc3a84d1
Priyanka-Kambli <88143725+Priyanka-Kambli@users.noreply.github.com>
2023-03-25 22:50:37 +0530

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,28 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive the first name and last name
+    public Email(String firstName,String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created :"+this.firstName +"" +this.lastName);
+    }
+
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set the mailboxCapacity
+
+    //Change the password
+
+
+}
---
+++ Emailapp.java
@@ -0,0 +1,7 @@
+package emailapp;
+
+public class Emailapp {
+    //class newobj = new class();
+    Email em1 = new Email("Priyanka","Kambli");
+
+}
